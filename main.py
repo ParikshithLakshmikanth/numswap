2083110343a4411449eb110dd555b7c32df363ab
ParikshithLakshmikanth <132085131+ParikshithLakshmikanth@users.noreply.github.com>
2024-09-22 18:06:14 +0530

    swaping numbers

---
+++ main.py
@@ -0,0 +1,9 @@
+x = int(input("Enter value for x:"))
+y = int(input("Enter value for y:"))
+
+temp = x
+x = y
+y = temp
+
+print("value of x after swapping:",x)
+print("value of y after swapping:",y)
